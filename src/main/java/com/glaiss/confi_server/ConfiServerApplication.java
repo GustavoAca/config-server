d4c0272ef04907a4fcea9a0e10fb4638c218045b
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-11-11 14:06:59 -0300

    first commit

---
+++ src/main/java/com/glaiss/confi_server/ConfiServerApplication.java
@@ -0,0 +1,15 @@
+package com.glaiss.confi_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glaiss/confi_server/ConfiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glaiss.confi_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
